f5a538e42f1e03800329ca8e290b852df167d6bc
Shadow007327 <soorushsalehirad@gmail.com>
2026-01-07 11:45:37 +0330

    Create Untils.py

---
+++ Untils.py
@@ -0,0 +1,11 @@
+def greet(name):
+    """Return a greeting message"""
+    return f"Hello, {name}!"
+
+def add(a, b):
+    """Return sum of two numbers"""
+    return a + b
+
+def multiply(a, b):
+    """Return product of two numbers"""
+    return a * b
